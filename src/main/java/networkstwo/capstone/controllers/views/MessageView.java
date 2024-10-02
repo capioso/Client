8e6c62e27c92f35fdbb7e1c9f8900a69498d0a66
Capi_oso <a.h.segales741@gmail.com>
2024-10-02 18:40:39 -0400

    updating

--- src/main/java/networkstwo/capstone/controllers/views/MessageView.java
+++ src/main/java/networkstwo/capstone/controllers/views/MessageView.java
@@ -1,6 +1,7 @@
 package networkstwo.capstone.controllers.views;
 
 import javafx.fxml.FXML;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.time.ZonedDateTime;
@@ -17,6 +18,15 @@ public class MessageView {
     @FXML
     private Text dateTimeText;
 
+    @FXML
+    public void initialize() {
+        Font titleFont = Font.loadFont(getClass().getResourceAsStream("/fonts/RobotoCondensed-MediumItalic.ttf"), 10);
+        usernameTitle.setFont(titleFont);
+        dateTimeText.setFont(titleFont);
+        Font bodyFont = Font.loadFont(getClass().getResourceAsStream("/fonts/RobotoCondensed-Medium.ttf"), 12);
+        messageBody.setFont(bodyFont);
+    }
+
     public void setMessageBody(String messageBody) {
         this.messageBody.setText(messageBody);
     }
@@ -26,7 +36,7 @@ public class MessageView {
     }
 
     public void setDateTime(ZonedDateTime dateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd-HH/mm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd-HH:mm");
         String formattedDate = dateTime.format(formatter);
         this.dateTimeText.setText(formattedDate);
     }
